e3df20d105a15362cb0e9f4a318e370028143712
JustinAmstadt <justinamstadt@gmail.com>
2024-10-18 00:52:12 -0700

    index bitmaps failing

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -8,12 +8,19 @@
 class ExperimentalCube {
     constructor(gl, vertexShader, fragmentShader) {
         vertexShader = `
+            uniform int indexBitmaps[12];
+
             uniform mat4 P;
             uniform mat4 MV;
 
+            out vec4 vColor;
+
             void main() {
+                // Shift to the right by a certain amount and then do a mask to find each value
+                int index = (indexBitmaps[gl_InstanceID] >> (gl_VertexID * 4)) & 0xF;
+
                 int bitmap = 15637664;
-                int bitmapShiftAmount = gl_VertexID * 3;
+                int bitmapShiftAmount = index * 3;
 
                 vec4 newPos;
                 newPos.x = ((bitmap & (1 << bitmapShiftAmount)) != 0) ? 0.5 : -0.5;
@@ -39,6 +46,7 @@ class ExperimentalCube {
             } 
         `;
         
+        /*
         let indices = new Uint16Array([
             // Front face
             0, 1, 2,
@@ -64,18 +72,26 @@ class ExperimentalCube {
             0, 2, 4,
             2, 6, 4 
         ]);
+        */
+
+        let indexBitmapArray = new Int16Array([
+            528, 561, 
+            1380, 1893,
+            
+        ]);
         
         let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
-        let indicesBuffer = new Indices(gl, indices);
+        // let indicesBuffer = new Indices(gl, indices);
+
+        const uniformLocation = gl.getUniformLocation(program.program, 'indexBitmaps');
 
         this.draw = () => {
             program.use();
 
-            indicesBuffer.enable()
+            gl.uniform1iv(uniformLocation, indexBitmapArray);
 
-            gl.drawElements(gl.TRIANGLES, indicesBuffer.count, indicesBuffer.type, 0);
-
-            indicesBuffer.disable()
+            // gl.drawElements(gl.TRIANGLES, indicesBuffer.count, indicesBuffer.type, 0);
+            gl.drawArraysInstanced(gl.TRIANGLES, 0, indexBitmapArray.length * 3, indexBitmapArray.length);
         };
     }
 };
